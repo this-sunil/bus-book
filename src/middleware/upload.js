f4fb0762638b151d5711deb666d04600813e274e
this-sunil <swarajya888@gmail.com>
2025-10-01 08:34:27 +0530

    submit

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -4,20 +4,20 @@ import fs from "fs";
 
 const uploadDir = path.join(process.cwd(), "/src/upload");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir, { recursive: true });
-    }
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    const timestamp = Date.now();
-    const ext = path.extname(file.originalname);
-    cb(null, `${timestamp}${ext}`);
-  }
-});
-
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     if (!fs.existsSync(uploadDir)) {
+//       fs.mkdirSync(uploadDir, { recursive: true });
+//     }
+//     cb(null, uploadDir);
+//   },
+//   filename: (req, file, cb) => {
+//     const timestamp = Date.now();
+//     const ext = path.extname(file.originalname);
+//     cb(null, `${timestamp}${ext}`);
+//   }
+// });
+const storage=multer.memoryStorage();
 const fileFilter = (req, file, cb) => {
   const allowedTypes = /jpeg|jpg|png|gif/;
   const mimeType = allowedTypes.test(file.mimetype);
@@ -29,8 +29,8 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
   storage,
-  fileFilter,
-  limits: { fileSize: 50 * 1024 * 1024 } 
+  // fileFilter,
+  // limits: { fileSize: 50 * 1024 * 1024 } 
 });
 
 export default upload;
