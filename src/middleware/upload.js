859fc22c7a6cf88f730957c4652d88ace2efe50b
this-sunil <swarajya888@gmail.com>
2025-10-01 08:30:56 +0530

    submit

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -2,25 +2,21 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-//const uploadDir = path.join(process.cwd(), "/src/upload");
+const uploadDir = path.join(process.cwd(), "/src/upload");
 
-const storage = multer.memoryStorage();
-
-/*multer.diskStorage({
-  
+const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir, { recursive: true });
     }
     cb(null, uploadDir);
   },
-  
   filename: (req, file, cb) => {
     const timestamp = Date.now();
     const ext = path.extname(file.originalname);
     cb(null, `${timestamp}${ext}`);
   }
-});*/
+});
 
 const fileFilter = (req, file, cb) => {
   const allowedTypes = /jpeg|jpg|png|gif/;
